663efc2fd271af02bb864a2c1cae8e0ebc6259cb
Guillaume RICHARD <g.jf.richard@gmail.com>
2016-07-25 21:40:35 +0200

    hierarchie plus complete avec Twig pour CodeIgniter

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+    public function __construct() {
+        parent::__construct();
+
+        $config = [
+            'paths' => ['application/views', VIEWPATH],
+            'cache' => 'application/cache',
+        ];
+        $this->load->library('twig', $config);
+    }
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        $data['css'][] = css_url('bootstrap.min');
+        $data['css'][] = css_url('font-awesome.min');
+        $data['css'][] = css_url('welcome/index');
+
+        $data['title'] = "Welcome to CodeIgniter !";
+        $data['text'] = "<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/Welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href=\"user_guide/\">User Guide</a>.</p>";
+
+        $this->twig->display('welcome_message', $data);
 	}
 }
